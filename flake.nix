87992a7c19b68ec20f035786db81c0fb7c435961
Endermanbugzjfc <175898536+Ezjfc@users.noreply.github.com>
2026-08-11 17:54:50 +1000

    chore: dev-shell improvement with recent Nixche

--- flake.nix
+++ flake.nix
@@ -6,13 +6,18 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixche.url = "github:ezjfc/nixche";
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    nixche,
   }: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = nixpkgs.legacyPackages.${system};
+    nixchePkgs = nixche.packages.${system};
+    inherit (nixchePkgs.write-alias-script) writeAliasScriptBin;
+    inherit (nixchePkgs.write-lua-script) writeLuaScriptShare;
 
     core = with pkgs; [
       nodejs
@@ -24,16 +29,22 @@
       tailwindcss = tailwindcss-language-server;
       nil = nil;
     };
+    launchScriptPkg = writeLuaScriptShare {
+      name = "neovim-launch-script";
+      inherit lsps;
+      text = builtins.readFile ./neovim/lsps.lua;
+      TYPESCRIPT = pkgs.typescript;
+    };
+    launchScript = "${launchScriptPkg}/share/${launchScriptPkg.name}.lua";
   in {
     devShells.core = pkgs.mkShellNoCC {
       packages = core;
     };
     devShells.nvim = pkgs.mkShellNoCC {
-      packages = builtins.attrValues lsps;
-
-      shellHook = ''
-        alias nvim="${pkgs.callPackage ./nix/nvim-conf/package.nix {}}"
-      '';
+      packages = core ++ (builtins.attrValues lsps) ++ [
+        launchScriptPkg
+        (writeAliasScriptBin "nvim" "nvim -c 'source ${launchScript}'")
+      ];
     };
   });
 }
---
+++ neovim/lsps.lua
@@ -0,0 +1,46 @@
+---@class Env
+---@field lsps { [string]: string }
+---@field TYPESCRIPT string
+
+---@type Env
+local env = ...
+
+for name, _ in pairs(env.lsps) do
+  vim.lsp.enable(name)
+end
+
+local tsdk = env.TYPESCRIPT .. "/lib/node_modules/typescript/lib"
+
+-- `@astrojs/ts-plugin` is not shipped by the astro-language-server derivation in a
+-- resolvable form (no `node_modules/@astrojs/ts-plugin` anywhere in it, and its own
+-- deps are missing), so it has to come from the project's own node_modules.
+local tsPluginLocation = vim.fs.joinpath(vim.fn.getcwd(), "node_modules/@astrojs/ts-plugin")
+if not vim.uv.fs_stat(tsPluginLocation) then
+  error("[neovim/lsps.lua] astrojs plugin for typescript-language-server required, run `npm i`")
+end
+vim.lsp.config("tl_ls", {
+  filetypes = {
+    "typescript",
+    "javascript",
+    "typescriptreact",
+    "javascriptreact",
+
+    "astro",
+  },
+  init_options = {
+    plugins = {
+      {
+        name = "@astrojs/ts-plugin",
+        location = tsPluginLocation,
+        languages = { "astro" },
+      },
+    },
+  },
+})
+
+vim.lsp.config("astro", {
+  init_options = {
+    typescript = { tsdk = tsdk },
+  },
+})
+
