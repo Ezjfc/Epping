1d0228ae1658d621770dced859b00b2dd87db3d7
EndermanbugZJFC <175898536+Ezjfc@users.noreply.github.com>
2026-08-23 01:06:22 +1000

    fix: hover effects not applying to tab focus

--- src/components/header/DropdownTrigger.tsx
+++ src/components/header/DropdownTrigger.tsx
@@ -1,38 +1,38 @@
-import { safePolygon, useFloating, useFocus, useHover, useInteractions, useRole } from "@floating-ui/react";
+import { safePolygon, useDismiss, useFloating, useFocus, useHover, useInteractions, useRole } from "@floating-ui/react";
 import { useState, type PropsWithChildren, type ReactNode } from "react";
-import type { CommonProps } from "../../utils/props";
+import type { CommonPropsReact } from "../../utils/props";
 
 export type Props = PropsWithChildren<PropsWc>
 
-interface PropsWc extends CommonProps {
+interface PropsWc extends CommonPropsReact {
   /**
    * menu is the tree of elements to display as the menu.
    */
   menu?: ReactNode
 }
 
-export default function Dropdown({ class: c, children, menu }: Props) {
+export default function Dropdown({ className, children, menu }: Props) {
   const [isOpen, setIsOpen] = useState(false)
   const { refs, floatingStyles, context } = useFloating({
     open: isOpen,
     onOpenChange: setIsOpen,
     placement: "bottom-start",
   })
-  const hover = useHover(context, { handleClose: safePolygon() })
+  const hover = useHover(context, { handleClose: safePolygon() }) // TODO: switch to floatingportal and focus manager
   const focus = useFocus(context)
   const role = useRole(context, { role: "menu" })
   const { getReferenceProps, getFloatingProps } = useInteractions([hover, focus, role])
 
   return (
-    <>
+    <div className={className} >
       {isOpen && (
-        <div ref={refs.setFloating} style={floatingStyles} {...getFloatingProps()}>
+        <div className="menu" ref={refs.setFloating} style={floatingStyles} {...getFloatingProps()}>
         {menu}
         </div>
       )}
-      <button className={`${c} cursor-pointer`} ref={refs.setReference} {...getReferenceProps()}>
+      <button className="summary cursor-pointer group" ref={refs.setReference} {...getReferenceProps()}>
       {children}
       </button>
-    </>
+    </div>
   )
 }
